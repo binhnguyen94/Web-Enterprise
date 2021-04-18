e20a527ad6dbf801071f4d539e1980c31f8f3c59
BinhNguyen <ntbinh200394@gmail.com>
2021-04-18 19:30:48 +0700

    edit file connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 ﻿<?php
 	mysql_connect("localhost","root","") or die("Internet connection error!");
-	mysql_select_db("database1") or die("Database connection error!");
+	mysql_select_db("database") or die("Database connection error!");
 	mysql_query('SET NAMES "UTF8"');
 ?>
